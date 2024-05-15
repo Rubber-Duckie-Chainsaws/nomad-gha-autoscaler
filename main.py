9a7bf5ae21d48e63af0b6e24eaf48fc067944eef
WigglesMcMuffin <teee.moose@gmail.com>
2024-05-15 15:47:17 -0400

    Switching to logger

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from flask import Flask, request
 import requests
-import pprint
+from flask import Flask, request
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 
@@ -8,16 +24,17 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Hello</h1><p>New World!</p>"
 
+#TODO: Secrets next
 @app.route("/github-webhook", methods=["POST"])
 def getWebhook():
-    print("Getting data as dictionary")
+    app.logger.info("Getting data as dictionary")
     data = dict(request.json)
-    print(data.get("action", ""))
+    app.logger.info(data.get("action", ""))
     if data.get("action", "") == "queued":
-        print("Dispatching runner")
+        app.logger.info("Dispatching runner")
         r = requests.post('http://10.0.1.81:4646/v1/job/github_runner/dispatch', data={})
-        print(r.text)
+        app.logger.info(r.text)
     else:
-        print("Doing nothing")
-        print(data.get("action", ""))
+        app.logger.info("Doing nothing")
+        app.logger.info(data.get("action", ""))
     return "ok"
