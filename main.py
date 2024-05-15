abf0764d9a3fc54ddb0807f83a64d31b29fdccf6
WigglesMcMuffin <teee.moose@gmail.com>
2024-05-15 15:22:20 -0400

    Log when action is queued

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import requests
 import pprint
 
 app = Flask(__name__)
@@ -6,11 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<h1>Hello</h1><p>New World!</p>"
-#    return "<h1>Hello</h1><p>World!</p>"
 
 @app.route("/github-webhook", methods=["POST"])
 def getWebhook():
-    data = request.data
-    pprint.pp(data)
+    data = dict(request.data)
+    if data.get("action", "") == "queued":
+        print("Dispatching runner")
+    else:
+        print("Doing nothing")
     return "ok"
-
